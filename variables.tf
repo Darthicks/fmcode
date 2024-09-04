adbcc47abe12dffccac802cc6f2c02813c55b639
Darthicks <153494439+Darthicks@users.noreply.github.com>
2024-09-04 15:49:27 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "The Azure region where resources will be deployed."
   type        = string
-  default     = "sweden"
+  default     = "swedensouth"
 }
  
 variable "vm_name" {
