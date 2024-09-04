e739dcc7840a805b3da5e7e3e076cc7a0c2ea6c6
Darthicks <153494439+Darthicks@users.noreply.github.com>
2024-09-04 15:03:36 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,61 +1,39 @@
 output "vm_id" {
-
   value = azurerm_virtual_machine.main.id
-
 }
- 
+
 output "aci_id" {
-
   value = azurerm_container_group.main.id
-
 }
- 
+
 output "key_vault_id" {
-
   value = azurerm_key_vault.main.id
-
 }
- 
+
 output "storage_account_id" {
-
   value = azurerm_storage_account.main.id
-
 }
- 
+
 output "app_gateway_id" {
-
   value = azurerm_application_gateway.main.id
-
 }
- 
-output "app_service_id" {
 
-  value = azurerm_app_service.main.id
-
+output "function_app_id" {
+  value = azurerm_function_app.main.id
 }
- 
+
 output "private_link_id" {
-
   value = azurerm_private_endpoint.main.id
-
 }
- 
+
 output "vnet_id" {
-
   value = azurerm_virtual_network.main.id
-
 }
- 
+
 output "subnet_id" {
-
   value = azurerm_subnet.main.id
-
 }
- 
+
 output "nsg_id" {
-
   value = azurerm_network_security_group.main.id
-
 }
-
- 
